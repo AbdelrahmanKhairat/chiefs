06110a4c151fc8a3ee026a70c7d2f2aa23bd3339
AbdelrahmanKhairat <155402844+AbdelrahmanKhairat@users.noreply.github.com>
2024-05-04 14:55:46 +0300

    last commit to merge

--- assets/js/script.js
+++ assets/js/script.js
@@ -40,40 +40,31 @@ $('.counting').each(function () {
 
 });
 
-
-const openModalButtons = document.querySelectorAll('[data-modal-target]')
-const closeModalButtons = document.querySelectorAll('[data-close-button]')
-const overlay = document.getElementById('overlay')
-
-openModalButtons.forEach(button => {
-  button.addEventListener('click', () => {
-    const modal = document.querySelector(button.dataset.modalTarget)
-    openModal(modal)
-  })
-})
-
-overlay.addEventListener('click', () => {
-  const modals = document.querySelectorAll('.modal.active')
-  modals.forEach(modal => {
-    closeModal(modal)
-  })
-})
-
-closeModalButtons.forEach(button => {
-  button.addEventListener('click', () => {
-    const modal = button.closest('.modal')
-    closeModal(modal)
-  })
-})
-
-function openModal(modal) {
-  if (modal == null) return
-  modal.classList.add('active')
-  overlay.classList.add('active')
+function deletePost(id) {
+  var div = document.getElementById(id);
+      if (div) {
+        div.parentNode.removeChild(div);
+      }
 }
 
-function closeModal(modal) {
-  if (modal == null) return
-  modal.classList.remove('active')
-  overlay.classList.remove('active')
+function updatePost(txtBoxDiv,id) {
+  // Get the input value from the text box
+  var inputText = document.getElementById(txtBoxDiv).value;
+
+  // Update the content of the div with the input value
+  document.getElementById(id).textContent = inputText;
+}
+
+var coll = document.getElementsByClassName("collapsible");
+
+for (var i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
 }
